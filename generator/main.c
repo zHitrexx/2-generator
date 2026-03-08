e039a3af7d2b6df2b7041d62ef6f2fdee2b4733c
zHitrexx <drahomir.pop99@gmail.com>
2026-03-08 23:28:12 +0100

    Enhance waveform generation and implement USART configuration

--- generator/main.c
+++ generator/main.c
@@ -2,7 +2,8 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 #include <math.h>
 #include "src/BitOps.h" // vložit src do Dokumenty -> Arduiono -> sketch složka -> src -> BitOps.h a .c
 #include "src/usart.h"
@@ -12,119 +13,165 @@
 #define CH1 PORTK
 #define CH2 PORTF
 
+uint8_t values1[4][256];
+uint8_t values2[4][256];
+char string[20];
+uint8_t index = 0;
 const uint8_t max_amp = 204;
-uint8_t values[4][max_amp];
-volatile uint8_t step_ch1 = 0;
-volatile uint8_t step_ch2 = 0;
-float amp = 4000;
-uint8_t amp_dac = 0;
-float freq1 = 600;
-float freq2 = 600;
+const uint32_t word_hz = 107374; // 1 Hz // 214748 -> 20 kHz base
+volatile uint32_t phase_ch1 = 0;
+volatile uint32_t phase_ch2 = 0;
+volatile uint32_t word_ch1 = 0;
+volatile uint32_t word_ch2 = 0;
+volatile uint8_t* table1 = values1[0];
+volatile uint8_t* table2 = values2[3];
 
 //-------Setup-------
 
-void Setup()
+void Setup(void)
 {
-  DDRCH1 = 0xFF;   // Nastavení PORTB na výstupní - CH1
-  DDRCH2 = 0xFF;   // Nastavení PORTK na výstupní - CH2
-  CH1 = 0x00;  // Nastavení log. 0 na celý PORTB
-  CH2 = 0x00;  // Nastavení log. 0 na celý PORTK
-  amp_dac = Amp(amp);
-}
-void SetupTimer()
-{
- TCCR1A = 0; // Ohlídání WGM10 a WGM11 LOW a zároveň COM1Xn v LOW (hw piny)
- TCCR1B = (1 << WGM12) | (1 << CS10); // Nastavení TIMER1 na CTC a Prescaler na 1
- OCR1A = 16000000 / (100.0 * 2.04 * freq1) - 1; // Nastavení na (147456MHz / 10 000 Hz - 1 = ?
- TIMSK1 = (1 << OCIE1A); // Povolit přerušení pri shodě s TCNT1 a OCR1A
-
- TCCR3A = 0;
- TCCR3B = (1 << WGM32) | (1 << CS30);
- OCR3A = 16000000 / (100.0 * 2.04 * freq2) - 1;
- TIMSK3 = (1 << OCIE3A);//ETIMSK = (1 << OCIE3A);
+  DDRCH1 = 0xFF;   // CH1 výstup
+  DDRCH2 = 0xFF;   // CH2 výstup
+  usart_setup(9600, bits8, parityNone, stop1); // V putty? nastavit stejné parametry
 }
 
-ISR(TIMER1_COMPA_vect) // Přerušení vyvolané TIMER1
+//-------Timer-------
+
+void SetupTimer(void)
 {
-  CH1 = values[0][step_ch1];
-  step_ch1++;
-  if (step_ch1 >= max_amp)
-	  step_ch1 = 0;
+ TCCR1A = 0;
+ TCCR1B = (1 << WGM12) | (1 << CS10);
+ OCR1A = 16000000 / 40000 - 1; // 147456MHz / 40 000 - 1 (20 000 když by to dělalo brikule ale zdvojnásobit word_hz)
+ TIMSK1 = (1 << OCIE1A); // TIMSK = (1 << OCIE1A);
 }
-ISR(TIMER3_COMPA_vect) // Přerušení vyvolané TIMER3
+
+//-------Interrupt-------
+
+ISR(TIMER1_COMPA_vect)
 {
-  CH2 = values[2][step_ch2];
-  step_ch2++;
-  if (step_ch2 >= max_amp)
-	  step_ch2 = 0;
+ phase_ch1 += word_ch1;
+ phase_ch2 += word_ch2;
+
+ CH1 = table1[phase_ch1 >> 24];
+ CH2 = table2[phase_ch2 >> 24];
 }
-uint8_t Amp(int amp)
+
+uint8_t Amp(float amp)
 {
-  if ((float)(amp) / 1000.0 > (float)max_amp / 51.0)   // 4500 [mV] / 1000 -> 4.5[V] > 4V
-    amp = (float)max_amp * 1000.0 / 51.0;                // 204 000 / 51 -> 4000[mV]
+  if (amp / 1000.0 > max_amp / 51)
+    amp = (float)max_amp * 1000.0 / 51.0;
   else if (amp < 0)
     amp = 0;
-
-  return (float)amp / 1000.0 * 51.0; // převedení z mV na V a potom na 8bitovou hodnotu (0 - 204)
+ 
+  return (float)amp / 1000.0 * 51.0;
 }
 
-float Period(float freq)
+void UpdateTable(uint8_t (*values)[256], uint8_t amp, uint32_t *word_ch, float freq)
 {
-  return 1.0 / freq; // f -> T 
-}
+  *word_ch = word_hz * freq;
+  float half_amp = (float)amp / 2.0;
+  float angle = 6.28318531 / 255.0; 
 
-void SetupValues()
-{
-  for (uint8_t i = 0; i < max_amp; i++)  // SQR
+  for (uint16_t i = 0; i < 256; i++)
   {
-    if (i < 102)
-      values[0][i] = amp_dac;
+	// SQR
+    if (i < 128)
+      values[0][i] = amp;
     else
       values[0][i] = 0; 
-  }
-  for (uint8_t i = 0; i < max_amp; i++) // SAW
-  {
-    values[1][i] = i;
-  }
-  for (uint8_t i = 0; i <= max_amp / 2; i++) // TRI
-  {
-	values[2][i] = i * 2;
-  }
-  for (uint8_t i = max_amp / 2; i < max_amp; i++)
-  {
-	values[2][i] = values[2][max_amp - i];
-  }
-  for (uint8_t i = 0; i < max_amp; i++)  // SIN
-  {
-    float angle = 6.28318531 * ((float)i / amp_dac);
-    values[3][i] = round((float)amp_dac / 2.0 * sin(angle) + (float)amp_dac / 2.0);
+
+	// SAW
+	values[1][i] = (i * amp) / 255;
+
+	// TRI
+	if (i < 128) 
+	  values[2][i] = (i * amp) / 127;
+	else
+	  values[2][i] = ((255 - i) * amp) / 128;
+	
+	// SIN
+    values[3][i] = round(half_amp * sin(i * angle) + half_amp);
   }
 }
 
-void Off()
+void ProcessUSART(void) // Zpracování příkazu přes USART
 {
-  CH1 = 0x00;
-  CH2 = 0x00;
+  char *channel  = strtok(string, ":"); // Rozklad na části
+  char *wave     = strtok(NULL, ":");
+  char *str_amp  = strtok(NULL, ":");
+  char *str_freq = strtok(NULL, ":");
+
+  if (channel == NULL || wave == NULL || str_amp == NULL || str_freq == NULL)
+	return;
+  
+  float amp = atoi(str_amp); // Převod na číslo
+  float freq = atoi(str_freq);
+  
+  uint8_t wave_index = 0;
+  if (strcmp(wave, "SQR") == 0)
+    wave_index = 0;
+  else if (strcmp(wave, "SAW") == 0)
+    wave_index = 1;
+  else if (strcmp(wave, "TRI") == 0)
+    wave_index = 2;
+  else if (strcmp(wave, "SIN") == 0)
+    wave_index = 3;
+  else
+  {
+	  printf("Spatny druh vlny!");
+  	return;
+  }
+  
+  if (strcmp(channel, "CH1") == 0)
+  {
+    UpdateTable(values1, Amp(amp), &word_ch1, freq);
+    table1 = values1[wave_index];
+  }
+  else if (strcmp(channel, "CH2") == 0)
+  {
+    UpdateTable(values2, Amp(amp), &word_ch2, freq);
+    table2 = values2[wave_index]; 
+  }
+  else
+  {
+    printf("Spatny kanal!");
+  }
 }
 
 //-------Main-------
- 
-
 
 int main()
 {
   Setup(); // Setup AVR PORT
   SetupTimer();
-  SetupValues();
   sei();
+  char znak;
 
-  while(true)
+  printf("Zadej prikaz ve formatu: channel:druh vlny:mV:Hz!\r\n");
+
+  while(1)
   {
-   // Vymyslet ten usart
-   // DDS?
-   // 
+	  if (usart_dataready())
+	  {
+	    znak = usart_getchar();
+	    if (znak == '\r' || znak == '\n')
+	    {
+	      if (index == 0)
+		      continue;
+			
+        string[index] = '\0';
+        ProcessUSART();
+        index = 0;
+	    }
+	    else
+	    {
+	      if (index < 19)
+		    {
+		      string[index] = znak;
+		      index++;
+	      }
+	    }
+	  }
   }
-
-  
   return 0;
 }
